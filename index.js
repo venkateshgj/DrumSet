50caa70cfc68fde62d5b7b5d95b9baaae8a683b5
venkateshgj <venkteshgj@gmail.com>
2023-11-20 13:38:56 +0530

    button animation added

--- index.js
+++ index.js
@@ -1,48 +1,24 @@
 for(var i =0; i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
         var buttonInnerHTML= this.innerHTML;
-        switch(buttonInnerHTML){
-            case 'w':
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            case 'a':
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-            case 's':
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-            case 'd':
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-            case 'j':
-                var bass = new Audio("sounds/kick-bass.mp3");
-                bass.play();
-                break;
-            case 'k':
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-            case 'l':
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
-            default:
-                console.log(buttonInnerHTML);
-        }
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener("keydown",function(event){
     var key = event.key;
+    makeSound(key);
+    buttonAnimation(key);
+});
+
+function makeSound(passKey){
+    var key = passKey;
     switch(key){
         case 'w':
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
         case 'a':
             var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
@@ -67,35 +43,15 @@ document.addEventListener("keydown",function(event){
             var crash = new Audio("sounds/crash.mp3");
             crash.play();
             break;
-        case 'W':
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            break;
-        case 'A':
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            break;
-        case 'S':
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            break;
-        case 'D':
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            break;
-        case 'J':
-            var bass = new Audio("sounds/kick-bass.mp3");
-            bass.play();
-            break;
-        case 'K':
-            var snare = new Audio("sounds/snare.mp3");
-            snare.play();
-            break;
-        case 'L':
-            var crash = new Audio("sounds/crash.mp3");
-            crash.play();
-            break;
         default:
-            console.log(event.key);
+            console.log(buttonInnerHTML);
     }
-});
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
